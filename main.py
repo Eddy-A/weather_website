8aee76af99df2f26327979b6e8e61576468b3aa1
Eddy-A <arred017@cougars.csusm.edu>
2023-09-14 18:58:25 -0700

    modified files

--- main.py
+++ main.py
@@ -1,36 +1,87 @@
+'''''
+# Define the JavaScript code as a string
+js_code = """
+document.addEventListener('DOMContentLoaded', function () {
+    const weatherForm = document.getElementById('weather-form');
+    const weatherInfo = document.getElementById('weather-info');
 
-import requests
+    weatherForm.addEventListener('submit', function (e) {
+        e.preventDefault();
 
-class Weather():
-    # Replace with your actual API key
-    api_key = '2e4968960047575c008c2c656e51bcc9'
-    
-    def __init__(self):
-        self.city = input("Enter the city: ")
-        self.country = input("Enter the country: ")
-        self.url = f'http://api.openweathermap.org/data/2.5/weather?q={self.city},{self.country}&appid={self.api_key}'
+        const cityInput = document.getElementById('city');
+        const countryInput = document.getElementById('country');
 
-    def api_request(self):
-        # Make the API request
-        response = requests.get(self.url)
-        data = response.json()
-        # Converting Kelvin to Fahrenheit
-        temp_kelvin = data['main']['temp']
-        temp_fahrenheit = (temp_kelvin - 273.15) * 9/5 + 32
-        humidity = data['main']['humidity']
-        wind_speed = data['wind']['speed']
-        return int(temp_fahrenheit), humidity, wind_speed
+        // Get user inputs
+        const city = cityInput.value;
+        const country = countryInput.value;
 
-    def display(self):
-        temp, humidity, wind_speed = self.api_request()
-        print("Weather in", self.city, self.country)
-        print("Temperature:", temp, "F")
-        print("Humidity:", humidity, "%")
-        print("Wind Speed:", wind_speed, "m/s")
+        // Replace with your actual API key
+        const apiKey = '2e4968960047575c008c2c656e51bcc9';
 
-    
-# Create an instance of the Weather class
-weather_instance = Weather()
+        // Construct the API URL
+        const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
 
-# Display weather information
-weather_instance.display()
+        // Fetch weather data
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                // Extract weather information
+                const tempKelvin = data.main.temp;
+                const tempFahrenheit = (tempKelvin - 273.15) * 9/5 + 32;
+                const humidity = data.main.humidity;
+                const windSpeed = data.wind.speed;
+
+                // Display weather information
+                weatherInfo.innerHTML = `
+                    <h2>Weather in ${city}, ${country}</h2>
+                    <p>Temperature: ${tempFahrenheit.toFixed(2)}°F</p>
+                    <p>Humidity: ${humidity}%</p>
+                    <p>Wind Speed: ${windSpeed} m/s</p>
+                `;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                weatherInfo.innerHTML = '<p>Failed to fetch weather data.</p>';
+            });
+    });
+});
+"""
+
+# Create an HTML file with the JavaScript code
+html_code = f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather App</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Weather App</h1>
+        <form id="weather-form">
+            <label for="city">City:</label>
+            <input type="text" id="city" name="city" required>
+            <label for="country">Country:</label>
+            <input type="text" id="country" name="country" required>
+            <button type="submit">Get Weather</button>
+        </form>
+        <div id="weather-info">
+            <!-- Weather data will be displayed here -->
+        </div>
+    </div>
+    <script>
+        {js_code}
+    </script>
+</body>
+</html>
+"""
+
+# Write the HTML code to a file
+with open('index.html', 'w') as file:
+    file.write(html_code)
+
+# Open the HTML file in the default web browser
+import webbrowser
+webbrowser.open('index.html')
+'''
--- script.js
+++ script.js
@@ -1,45 +1,29 @@
-// js/script.js
-document.addEventListener('DOMContentLoaded', function () {
-    const weatherForm = document.getElementById('weather-form');
-    const weatherInfo = document.getElementById('weather-info');
+// Replace this line with your API URL
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
 
-    weatherForm.addEventListener('submit', function (e) {
-        e.preventDefault();
+// Use a CORS proxy to fetch the data
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const finalUrl = proxyUrl + apiUrl;
 
-        const cityInput = document.getElementById('city');
-        const countryInput = document.getElementById('country');
+// Fetch weather data
+fetch(finalUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Extract weather information
+        const tempKelvin = data.main.temp;
+        const tempFahrenheit = (tempKelvin - 273.15) * 9/5 + 32;
+        const humidity = data.main.humidity;
+        const windSpeed = data.wind.speed;
 
-        // Get user inputs
-        const city = cityInput.value;
-        const country = countryInput.value;
-
-        // Replace with your actual API key
-        const apiKey = '2e4968960047575c008c2c656e51bcc9';
-
-        // Construct the API URL
-        const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
-
-        // Fetch weather data
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                // Extract weather information
-                const tempKelvin = data.main.temp;
-                const tempFahrenheit = (tempKelvin - 273.15) * 9/5 + 32;
-                const humidity = data.main.humidity;
-                const windSpeed = data.wind.speed;
-
-                // Display weather information
-                weatherInfo.innerHTML = `
-                    <h2>Weather in ${city}, ${country}</h2>
-                    <p>Temperature: ${tempFahrenheit.toFixed(2)}°F</p>
-                    <p>Humidity: ${humidity}%</p>
-                    <p>Wind Speed: ${windSpeed} m/s</p>
-                `;
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                weatherInfo.innerHTML = '<p>Failed to fetch weather data.</p>';
-            });
+        // Display weather information
+        weatherInfo.innerHTML = `
+            <h2>Weather in ${city}, ${country}</h2>
+            <p>Temperature: ${tempFahrenheit.toFixed(2)}°F</p>
+            <p>Humidity: ${humidity}%</p>
+            <p>Wind Speed: ${windSpeed} m/s</p>
+        `;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        weatherInfo.innerHTML = '<p>Failed to fetch weather data.</p>';
     });
-});
